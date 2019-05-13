0f7a5af9267ca8b52ef1320786419b0a19cf29bf
released <released@users.noreply.github.com>
2019-05-13 16:05:05 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# M480BSP_Coremark
---
+++ SampleCode/Template/core_portme.c
@@ -0,0 +1,257 @@
+/*
+Copyright 2018 Embedded Microprocessor Benchmark Consortium (EEMBC)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Original Author: Shay Gal-on
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "coremark.h"
+
+
+//porting Nuvoton start
+#include "NuMicro.h"
+
+void SYS_Init(void);
+
+#define SysTick_Counter_Disable        ((uint32_t)0xFFFFFFFE) 
+#define SysTick_Counter_Enable         ((uint32_t)0x00000001) 
+#define SysTick_Counter_Clear          ((uint32_t)0x00000000) 
+__IO uint32_t Tick; 	
+
+#define ITERATIONS      				(12000)		//12 sec
+
+//porting Nuvoton end
+
+#if VALIDATION_RUN
+	volatile ee_s32 seed1_volatile=0x3415;
+	volatile ee_s32 seed2_volatile=0x3415;
+	volatile ee_s32 seed3_volatile=0x66;
+#endif
+#if PERFORMANCE_RUN
+	volatile ee_s32 seed1_volatile=0x0;
+	volatile ee_s32 seed2_volatile=0x0;
+	volatile ee_s32 seed3_volatile=0x66;
+#endif
+#if PROFILE_RUN
+	volatile ee_s32 seed1_volatile=0x8;
+	volatile ee_s32 seed2_volatile=0x8;
+	volatile ee_s32 seed3_volatile=0x8;
+#endif
+	volatile ee_s32 seed4_volatile=ITERATIONS;
+	volatile ee_s32 seed5_volatile=0;
+/* Porting : Timing functions
+	How to capture time and convert to seconds must be ported to whatever is supported by the platform.
+	e.g. Read value from on board RTC, read value from cpu clock cycles performance counter etc. 
+	Sample implementation for standard time.h and windows.h definitions included.
+*/
+/* Define : TIMER_RES_DIVIDER
+	Divider to trade off timer resolution and total time that can be measured.
+
+	Use lower values to increase resolution, but make sure that overflow does not occur.
+	If there are issues with the return value overflowing, increase this value.
+	*/
+
+#if 1
+//porting Nuvoton start
+#define EE_TICKS_PER_SEC (1000)
+
+//porting Nuvoton end
+#else
+//#define NSECS_PER_SEC CLOCKS_PER_SEC
+//#define CORETIMETYPE clock_t 
+//#define GETMYTIME(_t) (*_t=clock())
+//#define MYTIMEDIFF(fin,ini) ((fin)-(ini))
+//#define TIMER_RES_DIVIDER 1
+//#define SAMPLE_TIME_IMPLEMENTATION 1
+//#define EE_TICKS_PER_SEC (NSECS_PER_SEC / TIMER_RES_DIVIDER)
+
+/** Define Host specific (POSIX), or target specific global time variables. */
+//static CORETIMETYPE start_time_val, stop_time_val;
+#endif
+
+/* Function : start_time
+	This function will be called right before starting the timed portion of the benchmark.
+
+	Implementation may be capturing a system timer (as implemented in the example code) 
+	or zeroing some system parameters - e.g. setting the cpu clocks cycles to 0.
+*/
+void start_time(void) {
+
+#if 1
+//porting Nuvoton start
+	Tick = 0; 
+	SysTick_Config(SystemCoreClock/1000);
+
+//porting Nuvoton end
+#else
+	GETMYTIME(&start_time_val );      
+#endif
+}
+/* Function : stop_time
+	This function will be called right after ending the timed portion of the benchmark.
+
+	Implementation may be capturing a system timer (as implemented in the example code) 
+	or other system parameters - e.g. reading the current value of cpu cycles counter.
+*/
+void stop_time(void) {
+
+#if 1
+//porting Nuvoton start
+
+	/* Stop the Timer and get the encoding time  */
+	SysTick->CTRL &= SysTick_Counter_Disable;
+
+	/* Clear the SysTick Counter */ 
+	SysTick->VAL = SysTick_Counter_Clear; 
+
+//porting Nuvoton end
+#else
+	GETMYTIME(&stop_time_val );      
+#endif
+}
+/* Function : get_time
+	Return an abstract "ticks" number that signifies time on the system.
+	
+	Actual value returned may be cpu cycles, milliseconds or any other value,
+	as long as it can be converted to seconds by <time_in_secs>.
+	This methodology is taken to accomodate any hardware or simulated platform.
+	The sample implementation returns millisecs by default, 
+	and the resolution is controlled by <TIMER_RES_DIVIDER>
+*/
+CORE_TICKS get_time(void) {
+
+#if 1
+//porting Nuvoton start
+
+	CORE_TICKS  elapsed = (CORE_TICKS)Tick; 
+	return elapsed;
+
+#else
+	CORE_TICKS elapsed=(CORE_TICKS)(MYTIMEDIFF(stop_time_val, start_time_val));
+	return elapsed;
+#endif
+
+}
+/* Function : time_in_secs
+	Convert the value returned by get_time to seconds.
+
+	The <secs_ret> type is used to accomodate systems with no support for floating point.
+	Default implementation implemented by the EE_TICKS_PER_SEC macro above.
+*/
+secs_ret time_in_secs(CORE_TICKS ticks) {
+	secs_ret retval=((secs_ret)ticks) / (secs_ret)EE_TICKS_PER_SEC;
+	return retval;
+}
+
+ee_u32 default_num_contexts=1;
+
+/* Function : portable_init
+	Target specific initialization code 
+	Test for some common mistakes.
+*/
+void portable_init(core_portable *p, int *argc, char *argv[])
+{
+
+
+//porting Nuvoton start
+    SYS_Init();
+    /* Init UART to 115200-8n1 for print message */
+    UART_Open(UART0, 115200);
+    /* Connect UART to PC, and open a terminal tool to receive following message */
+
+	printf("\r\nCLK_GetCPUFreq : %8d\r\n",CLK_GetCPUFreq());
+	printf("CLK_GetHCLKFreq : %8d\r\n",CLK_GetHCLKFreq());	
+	printf("CLK_GetPCLK0Freq : %8d\r\n",CLK_GetPCLK0Freq());
+	printf("CLK_GetPCLK1Freq : %8d\r\n",CLK_GetPCLK1Freq());
+
+    printf("Coremark start (%d ms) !\n" , ITERATIONS);
+
+
+	
+//porting Nuvoton end
+
+
+	if (sizeof(ee_ptr_int) != sizeof(ee_u8 *)) {
+		ee_printf("ERROR! Please define ee_ptr_int to a type that holds a pointer!\n");
+	}
+	if (sizeof(ee_u32) != 4) {
+		ee_printf("ERROR! Please define ee_u32 to a 32b unsigned type!\n");
+	}
+	p->portable_id=1;
+}
+/* Function : portable_fini
+	Target specific final code 
+*/
+void portable_fini(core_portable *p)
+{
+	p->portable_id=0;
+}
+
+//porting Nuvoton start
+void SYS_Init(void)
+{
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init System Clock                                                                                       */
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+
+    /* Set XT1_OUT(PF.2) and XT1_IN(PF.3) to input mode */
+    PF->MODE &= ~(GPIO_MODE_MODE2_Msk | GPIO_MODE_MODE3_Msk);
+
+    /* Enable External XTAL (4~24 MHz) */
+    CLK_EnableXtalRC(CLK_PWRCTL_HXTEN_Msk);
+
+    /* Waiting for 12MHz clock ready */
+    CLK_WaitClockReady(CLK_STATUS_HXTSTB_Msk);
+
+    /* Set core clock as PLL_CLOCK from PLL */
+    CLK_SetCoreClock(FREQ_192MHZ);
+    /* Set PCLK0/PCLK1 to HCLK/2 */
+    CLK->PCLKDIV = (CLK_PCLKDIV_PCLK0DIV2 | CLK_PCLKDIV_PCLK1DIV2);
+
+    /* Enable UART clock */
+    CLK_EnableModuleClock(UART0_MODULE);
+
+    /* Select UART clock source from HXT */
+    CLK_SetModuleClock(UART0_MODULE, CLK_CLKSEL1_UART0SEL_HXT, CLK_CLKDIV0_UART0(1));
+
+    /* Update System Core Clock */
+    /* User can use SystemCoreClockUpdate() to calculate SystemCoreClock. */
+    SystemCoreClockUpdate();
+
+
+
+    /* Set GPB multi-function pins for UART0 RXD and TXD */
+    SYS->GPB_MFPH &= ~(SYS_GPB_MFPH_PB12MFP_Msk | SYS_GPB_MFPH_PB13MFP_Msk);
+    SYS->GPB_MFPH |= (SYS_GPB_MFPH_PB12MFP_UART0_RXD | SYS_GPB_MFPH_PB13MFP_UART0_TXD);
+    /* Lock protected registers */
+    SYS_LockReg();
+}
+
+
+void SysTick_Handler(void) 
+{ 
+	extern __IO uint32_t Tick; 
+	Tick++; 
+} 
+
+
+//porting Nuvoton end
+
+
+
+
